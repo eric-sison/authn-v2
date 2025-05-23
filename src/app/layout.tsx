0f4f30365742f5bbafb803295152e8a08f4d248b
eric-sison <ericsison.dev@gmail.com>
2025-05-23 16:10:32 +0800

    chore: code refactoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from "next";
+import { type Metadata } from "next";
+import { type PropsWithChildren } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { PropsWithChildren } from "react";
 
 const inter = Inter({
   variable: "--font-inter",
